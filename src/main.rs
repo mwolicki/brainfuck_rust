368bdf33a31142e07a7a07bbce59b60faf46ef5a
Marcin Wolicki <marcin.wolicki@gmail.com>
2017-10-04 22:28:38 +0100

    reduced copying

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::collections::HashMap;
-use std::io::{self, Read, Write};
+use std::io::{self, Read};
 use std::num::Wrapping;
 
 #[derive(Debug)]
@@ -13,10 +13,11 @@ enum Op {
     While { ops: Vec<Op> },
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 struct State {
     curr_ptr: i32,
     data: HashMap<i32, u8>,
+    output: String,
 }
 
 fn read() -> u8 {
@@ -25,47 +26,40 @@ fn read() -> u8 {
     buffer[0]
 }
 
-fn print(ch: u8) {
-    io::stdout().write(&[ch]).ok();
-}
-
-fn eval_while(s: State, ops: &[Op]) -> State {
-    let mut state = s;
+fn eval_while(state: &mut State, ops: &[Op]) {
     while *(state.data.entry(state.curr_ptr).or_insert(0)) != 0 {
-        state = eval_vec(state, ops);
+        eval_vec(state, ops);
     }
-    state
 }
 
-fn eval_vec(s: State, ops: &[Op]) -> State {
-    let mut state = s;
+fn eval_vec(state: &mut State, ops: &[Op]) {
     for op in ops.iter() {
-        state = eval(state, op);
+        eval(state, op);
     }
-    state
 }
 
-fn eval(mut s: State, op: &Op) -> State {
-    {
-        let state = s.clone();
-        let val = s.data.entry(s.curr_ptr).or_insert(0);
-        let wrapped_val = Wrapping(*val);
-        let wrapped_one = Wrapping(1);
-
-        match *op {
-            Op::IncPointer => s.curr_ptr += 1,
-            Op::DecPointer => s.curr_ptr -= 1,
-            Op::IncVal => *val = (wrapped_val + wrapped_one).0,
-            Op::DecVal => *val = (wrapped_val - wrapped_one).0,
-            Op::Print => print(*val),
-            Op::Read => *val = read(),
-            Op::While { ref ops } => {
-                return eval_while(state, ops);
+fn eval(state: &mut State, op: &Op) {
+    match *op {
+        Op::IncPointer => state.curr_ptr += 1,
+        Op::DecPointer => state.curr_ptr -= 1,
+        Op::While { ref ops } => {
+            eval_while(state, ops);
+        }
+        _ => {
+            let val = state.data.entry(state.curr_ptr).or_insert(0);
+            let wrapped_val = Wrapping(*val);
+            let wrapped_one = Wrapping(1);
+            match *op {
+                Op::IncVal => *val = (wrapped_val + wrapped_one).0,
+                Op::DecVal => *val = (wrapped_val - wrapped_one).0,
+                Op::Print => state.output.push(char::from(*val)),
+                Op::Read => *val = read(),
+                _ => panic!("impossible!"),
             }
         }
     }
-    s
 }
+
 
 fn get_ast(code: &str) -> (Vec<Op>, usize) {
     let mut ops = Vec::new();
@@ -98,56 +92,22 @@ fn get_ast(code: &str) -> (Vec<Op>, usize) {
     (ops, i)
 }
 
-fn main() {
-    let hello_word = " 
-[ This program prints 'Hello World!' and a newline to the screen, its
-  length is 106 active command characters. [It is not the shortest.]
-
-  This loop is an \"initial comment loop\", a simple way of adding a comment
-  to a BF program such that you don't have to worry about any command
-  characters. Any .,+-<> characters are simply
-  ignored, the '[' and ']' characters just have to be balanced. This
-  loop and the commands it contains are ignored because the current cell
-  defaults to a value of 0; the 0 value causes this loop to be skipped.
-]
-++++++++               Set Cell #0 to 8
-[
-    >++++               Add 4 to Cell #1; this will always set Cell #1 to 4
-    [                   as the cell will be cleared by the loop
-        >++             Add 2 to Cell #2
-        >+++            Add 3 to Cell #3
-        >+++            Add 3 to Cell #4
-        >+              Add 1 to Cell #5
-        <<<<-           Decrement the loop counter in Cell #1
-    ]                   Loop till Cell #1 is zero; number of iterations is 4
-    >+                  Add 1 to Cell #2
-    >+                  Add 1 to Cell #3
-    >-                  Subtract 1 from Cell #4
-    >>+                 Add 1 to Cell #6
-    [<]                 Move back to the first zero cell you find; this will
-                        be Cell #1 which was cleared by the previous loop
-    <-                  Decrement the loop Counter in Cell #0
-]                       Loop till Cell #0 is zero; number of iterations is 8
-
-The result of this is:
-Cell No :   0   1   2   3   4   5   6
-Contents:   0   0  72 104  88  32   8
-Pointer :   ^
-
->>.                     Cell #2 has value 72 which is 'H'
->---.                   Subtract 3 from Cell #3 to get 101 which is 'e'
-+++++++..+++.           Likewise for 'llo' from Cell #3
->>.                     Cell #5 is 32 for the space
-<-.                     Subtract 1 from Cell #4 for 87 to give a 'W'
-<.                      Cell #3 was set to 'o' from the end of 'Hello'
-+++.------.--------.    Cell #3 for 'rl' and 'd'
->>+.                    Add 1 to Cell #5 gives us an exclamation point
->++.                    And finally a newline from Cell #6";
-
-    let init_state = State {
+fn run_brainfuck(code: &str) -> String {
+    let mut state = State {
         curr_ptr: 0,
         data: HashMap::new(),
+        output: String::new(),
     };
-    let (ast, _) = get_ast(hello_word);
-    eval_vec(init_state, &ast);
+    let (ast, _) = get_ast(code);
+    eval_vec(&mut state, &ast);
+    state.output
+}
+
+fn main() {
+    let result = run_brainfuck(
+        "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++.. +++.>++.<<+++++++++++++++.>.+++.------.--------.>+.",
+    );
+    println!("{}", result)
+
+
 }
